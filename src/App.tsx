6988eccac64717dcabd37fe4f5e70de58ca7ec29
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2024-01-20 16:30:11 -0800

    fix re-rendering component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { Mappedin, TGetVenueMakerOptions } from "@mappedin/mappedin-js";
-import "@mappedin/mappedin-js/lib/mappedin.css";
-import "./App.css";
-import useMapView from "./hooks/useMapView";
-import useVenue from "./hooks/useVenue";
-import React from 'react';
+import Map from "./Map";
+// import { DialogDemo } from "./components/DialogDemo";
+import "../app/globals.css";
 
 export default function App() {
-	// See Trial API key Terms and Conditions
-	// https://developer.mappedin.com/api-keys
-	console.log(import.meta.env);
-	const options: TGetVenueMakerOptions = {
-		mapId: "65ac3a16ca641a9a1399dc24",
-		key: import.meta.env.VITE_MI_CLIENT_ID,
-		secret: import.meta.env.VITE_MI_CLIENT_SECRET,
-	};
-
-	const venue: Mappedin | undefined = useVenue(options);
-	const { elementRef, mapView } = useMapView(venue);
-
-	console.log(mapView);
-
-	return <div id="app" ref={elementRef} />;
+	return (
+		<div id="app">
+			<Map />
+		</div>
+	);
 }
